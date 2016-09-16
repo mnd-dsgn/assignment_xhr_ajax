73f40f7556b785660f54b5a092d825af44b58b04
“Matthew <matthew.hinea@gmail.com>
2016-09-16 12:00:55 -0700

    finish

--- script.js
+++ script.js
@@ -8,11 +8,13 @@
 // xhr.send();
 
 function jsonToQueryString(json) {
-    return Object.keys(json).map(function(key) {
+    return "?" + Object.keys(json).map(function(key) {
       return encodeURIComponent(key) + '=' +
       encodeURIComponent(json[key]);
     }).join('&');
   };
+
+
 
 var $ = (function(){
 
@@ -20,6 +22,12 @@ var $ = (function(){
     var xhr = new XMLHttpRequest();
 
     var async = data.async || true; 
+
+    if(data.type = "GET") {
+      data.url += jsonToQueryString(data.data);
+      console.log(data.url);
+    }    
+
     xhr.open(data.type, data.url, async);
 
     if (data.headers) {
@@ -47,57 +55,81 @@ var $ = (function(){
     if (data.type === "POST") {
       xhr.send(jsonToQueryString(data.data));
     } else {
-      xhr.send(jsonToQueryString(data.data));
+      xhr.send();
     }
   };
 
+  var get = function(url, data, success, dataType) {
+    var data = {
+      type: "GET",
+      url: url,
+      data: data,
+      success: success,
+      headers: {"Data-Type": dataType}
+    }
+    ajax(data);
+  };
+
+  var post = function(url, data, success, dataType) {
+    var data = {
+      type: "POST",
+      url: url,
+      data: data,
+      success: success,
+      headers: {"Data-Type": dataType}
+    }
+    ajax(data);
+  };
+
+
   return {
     ajax: ajax,
+    get: get,
+    post: post
   }
+
 })();
 
-
-
-$.ajax({
-  url: "http://reqres.in/api/users",
-  type: "GET",
-
-  data: {
-    page: 2,
-  },
-
-  headers: {
-    "Data-Type": 'json',
-    "Content-Type" : 'application/json' 
-  },
-
-
-  success: function( json ) {
+$.get("http://reqres.in/api/users",  {page: 3}, function( json ) {
     console.log(JSON.parse(json));
-    // for example, build a post object onto the body
-  },
+  }, "application/json");
 
-  // Error callback to run if the request fails
-  // (e.g. server returns an error code like 301)
-  // The raw request and any status codes are 
-  // passed to the callback
-  error: function( xhr, status, errorThrown ) {
-      alert( "Sorry, there was a problem!" );
-      console.log( "Error: " + errorThrown );
-      console.log( "Status: " + status );
-      console.dir( xhr );
-  },
+$.post("http://reqres.in/api/users",  { "name": "adrian", "job":"roll shoulders" }, function( json ) {
+    console.log(JSON.parse(json));
+  }, "application/json");
 
-  // Complete callback to run regardless of the outcome
-  complete: function( xhr, status ) {
-      alert( "The request is complete!" );
-  }
-});
+// $.ajax({
+//   url: "http://reqres.in/api/users",
+//   type: "GET",
 
-// // Create a post
-// var xhr = new XMLHttpRequest();
-// xhr.addEventListener( "load", function(){
-//     console.log( this.responseText );
+//   data: {
+//     page: 2,
+//   },
+
+//   headers: {
+//     "Data-Type": 'json',
+//     "Content-Type" : 'application/json' 
+//   },
+
+
+//   success: function( json ) {
+//     //console.log(JSON.parse(json));
+//     // for example, build a post object onto the body
+//   },
+
+//   // Error callback to run if the request fails
+//   // (e.g. server returns an error code like 301)
+//   // The raw request and any status codes are 
+//   // passed to the callback
+//   error: function( xhr, status, errorThrown ) {
+//       alert( "Sorry, there was a problem!" );
+//       // console.log( "Error: " + errorThrown );
+//       // console.log( "Status: " + status );
+//       // console.dir( xhr );
+//   },
+
+//   // Complete callback to run regardless of the outcome
+//   complete: function( xhr, status ) {
+//       alert( "The request is complete!" );
+//   }
 // });
-// xhr.open("POST", "http://reqres.in/api/posts", true);
-// xhr.send("title=Foo&body=Bar&userId=1");
